03cb3e004360bb0248b5216020cf4cf4d294b411
SaiDictator <141777716+SaiDictator@users.noreply.github.com>
2023-09-14 23:44:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 from os import getenv
-from data import THE_ALTS
+from data import THE_SAIF
 
 
 #----------------------------------- REQUIRED CODES --------------------------------------#
 
-API_ID = int(getenv("API_ID", "25981592"))
-API_HASH = getenv("API_HASH", "709f3c9d34d83873d3c7e76cdd75b866")
+API_ID = int(getenv("API_ID", "25899888)
+API_HASH = getenv("API_HASH", "125b1d5e3277ab423dc98bbab94f9941)
 SESSION1 = getenv("SESSION")
-ALIVE_PIC = getenv("ALIVE_PIC", "https://te.legra.ph/file/07d39b85c6cea32f15259.jpg")
-OWNER_ID = int(getenv("OWNER_ID", "5518687442"))
+ALIVE_PIC = getenv("ALIVE_PIC", "https://telegra.ph/file/dafe713ade60212c37933.jpg")
+OWNER_ID = int(getenv("OWNER_ID", "6697540778"))
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
@@ -25,10 +25,10 @@ SESSION8 = getenv("SESSION8")
 SESSION9 = getenv("SESSION9")
 SESSION10 = getenv("SESSION10")
 
-SUDO_USERS = list(map(lambda x: int(x), getenv("SUDO_USERS", "5518687442").split(" ")))
+SUDO_USERS = list(map(lambda x: int(x), getenv("SUDO_USERS", "6585111267").split(" ")))
 SUDO_USERS.append(OWNER_ID)
 
-for x in THE_ALTS:
+for x in THE_SAIF:
     SUDO_USERS.append(x)
 
 SESSIONS = [SESSION1, SESSION2, SESSION3, SESSION4, SESSION5, SESSION6, SESSION7, SESSION8, SESSION9, SESSION10]
